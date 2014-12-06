b84f1aaa80df52b21924b8955d299614ef545dff
Xiaoli Shen <alabebop@gmail.com>
2014-12-06 21:26:16 +0100

    add boot load preload assets sample code and create code

--- js/alabebop/states/01-boot.js
+++ js/alabebop/states/01-boot.js
@@ -4,12 +4,19 @@ alabebop.BootState.prototype = {
 
     preload : function () {
         //TODO: load preloader assets
+        //this.load.image('preloaderBg', 'img/loading-bg.png');
+        //this.load.image('preloaderBar', 'img/loading-bar.png');
     },
 
     create : function () {
         //TODO: setup game environment, input, etc...
-
+        this.game.input.maxPointers = 1;
+        this.game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
+        this.game.stage.scale.pageAlignHorizontally = true;
+        this.game.stage.scale.pageAlignVertically = true;
+        this.game.stage.scale.setScreenSize(true);
         this.game.state.start('preload');
+
     }
 
 }
