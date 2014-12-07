f71b9cda3178dc7f76eb528a57a4d61c0cf9b7cd
Xiaoli Shen <alabebop@gmail.com>
2014-12-07 22:11:11 +0100

    add initial x velocity to figure

--- js/alabebop/game_setting.js
+++ js/alabebop/game_setting.js
@@ -4,8 +4,9 @@ alabebop.GameSetting = function() {
 
     this.setting = {
         totalLevels : 5,
-        totalFigures : 10
-
+        totalFigures : 10,
+        numPlanks : [4, 5, 6, 7, 3],
+        gridSizes : [100, 80, 60, 60, 200]
     }
 
     this.levelData = {
--- js/alabebop/states/07-level_round.js
+++ js/alabebop/states/07-level_round.js
@@ -11,8 +11,10 @@ alabebop.LevelRoundState.prototype = {
         //enable physics
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity.y = 150;
+
         //add background
         this.game.add.sprite(0, 0, 'castle');
+
         //create
         this.createFigures();
         this.createPlanks();
@@ -23,7 +25,7 @@ alabebop.LevelRoundState.prototype = {
 
     update: function() {
 
-
+        this.game.physics.arcade.collide(this.figures, this.planks)
 
     },
 
@@ -35,6 +37,51 @@ alabebop.LevelRoundState.prototype = {
         this.createNewFigure();
 
         this.generateFigureLoop = this.game.time.events.loop(Phaser.Timer.SECOND * 3, this.createNewFigure, this);
+
+    },
+
+    createPlanks: function() {
+        this.planks = this.game.add.group();
+        this.planks.enableBody = true;
+
+        for ( var i = 0; i < this.game.gameSetting.setting.numPlanks[this.levelData.level]; i++) {
+
+            this.createNewPlank(i)
+
+        }
+
+    },
+
+    createPools: function() {
+
+    },
+
+    createNewPlank : function(index) {
+        var grid = this.calcGrid(index);
+        var x = index === 0 ? this.game.world.centerX :
+            grid.minX + Math.ceil(Math.random() * this.game.gameSetting.setting.gridSizes[this.levelData.level]);
+        var y = index === 0 ? 200 :
+            grid.minY + Math.ceil(Math.random() * this.game.gameSetting.setting.gridSizes[this.levelData.level]);
+
+        var newPlank = this.planks.create( x, y, 'plank');
+        newPlank.anchor.setTo(0.5, 0.5);
+        newPlank.body.allowGravity = false;
+        newPlank.body.immovable = true;
+    },
+
+    calcGrid : function(index) {
+        var paddingLeft,
+            paddingRight = paddingLeft = 70,
+            paddingTop = 200,
+            paddingBottom = 260,
+            gridSize = this.game.gameSetting.setting.gridSizes[this.levelData.level],
+            grid = {
+                minX : paddingLeft,
+                minY : paddingTop
+            };
+
+        return grid;
+
 
     },
 
@@ -56,8 +103,9 @@ alabebop.LevelRoundState.prototype = {
             }
 
             newFigure.frame = 5;
+            newFigure.body.allowGravity = false;
             newFigure.body.collideWorldBounds = true;
-            newFigure.timer = this.game.time.events.add(Phaser.Timer.SECOND, this.jump, newFigure);
+            newFigure.timer = this.game.time.events.add(Phaser.Timer.SECOND * 2, this.jumpFigure, this, newFigure);
 
         } else {
             //remove generate figure timer
@@ -67,19 +115,15 @@ alabebop.LevelRoundState.prototype = {
 
     },
 
-    //this is pointed to the figure sprite
-    jump: function() {
-        //this.game.physics.enable(newFigure, Phaser.Physics.ARCADE);
-        this.body.bounce.y = 0.5 + Math.ceil(Math.random()) * .3;
-        this.body.bounce.x = 0.5 + Math.ceil(Math.random()) * .3;
-    },
-
-    createPlanks: function() {
-
-    },
-
-    createPools: function() {
-
+    jumpFigure: function(figure) {
+        figure.frame = 4;
+        figure.body.velocity.x = (Math.random() -.5) * 400;
+        figure.body.allowGravity = true;
+        figure.body.bounce.y = 0.5 + Math.ceil(Math.random()) * .3;
+        figure.body.bounce.x = 0.5 + Math.ceil(Math.random()) * .3;
+        this.game.time.events.remove(figure.timer);
     }
+
+
 
 }
